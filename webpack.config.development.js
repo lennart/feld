e72dcf2f8e0bf829f761ee93f84bec9f5b251737
Lennart Melzer <lennart@melzer.it>
2021-04-14 09:48:32 +0200

    move dev api config to webpack layer

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -3,7 +3,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 config = require('./webpack.config')
 
-config.plugins.push(new HtmlWebpackPlugin({}))
+config.plugins.push(new HtmlWebpackPlugin({
+  templateParameters: {
+    apiUrl: 'https://127.0.0.1:5000',
+    progressionId: 1
+  }
+}))
 config.output.filename = "index.js"
 
 config.devServer = {
